57c145d30d1c7439e919d1d18dbc0bb90119b82f
javohir1606 <javohirallayorov1606@gmail.com>
2024-08-05 00:25:56 +0500

    misol

---
+++ script.js
@@ -0,0 +1,194 @@
+// let a = 12;
+// let b = a % 10;
+// let c = (a - b)/10 
+// let d = b * 10 + c
+// console.log(d);
+//
+// 7 - misol
+// let a = 77;
+// let b = 11;
+// let c =a;
+ 
+// a = b;
+// b = c;
+// console.log(a);
+// console.log(b);
+
+// 8 - misol
+// let a = 77;
+// let b = 11;
+// if(a != b) {
+//   console.log(a + b);
+  
+// }else{
+//   console.log(0)
+// }
+
+// 9 - misol
+// let a = 12;
+// let b = 13;
+// let c = 10;
+// if ((a > b && a < c) || (a < b && a > c)){
+//   console.log(a);
+// }else if ((b > a && b < c) || (b < a && b > c)){
+//   console.log(b);
+// }else{
+// console.log(c);
+// }
+
+// 10 - misol
+// let a = 20;
+// let b = 10;
+// if (a != b){
+//   console.log(a + b);
+  
+// }else{
+//   console.log(0);
+  
+// }
+
+// 11 - misol
+// let a = 10;
+// let b = 5;
+// if (a )
+
+// 11 - misol
+// let a = 15;
+// let b = 15;
+// let c = 25;
+// if (a + b > a + c && a + b > c + b){
+//   console.log(a + b);
+  
+// }else if(b + c > a + b && b + c > c + a){
+//   console.log(b + c);
+  
+// }else{
+//   console.log(c + a)
+// }
+
+// let a = 6;
+// let b = 11;
+// if(a == 6 || b == 6 || a + b == 6){
+//   console.log(true);
+// }else{
+//   console.log(false);
+  
+// }
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// if((a < b && b < c) || (a > b && b > c)){
+//   console.log(true);
+// }else{
+//   console.log(false);
+// }
+
+// let a = 5;
+// let b = 12;
+// let c = 19;
+// if(a > 10 || b > 10 || c > 10){
+//   console.log(true);
+// }else{
+//   console.log(false);
+// }
+
+
+
+
+
+
+// Uygi vazifa
+
+// 1 - misol
+// let a = 2;
+// if (a > 0){
+//   console.log(a + 1);
+// }else{
+//   console.log(a);
+// }
+
+// 2 - misol
+// let a = -5;
+// if (a > 0){
+//   console.log(a + 1);
+// }else{
+//   console.log(a + 2);
+// }
+
+// 3 - misol
+// let a = 5;
+// if (a > 0){
+//   console.log(a + 1)
+// }else if(a < 0){
+//   console.log(a + 2);
+// }else{
+//   console.log(10);
+  
+// }
+
+// 4 - misol
+// let a = -5;
+// let b = 10;
+// let c = 12;
+// let count =0
+// if (a > 0){
+// }  else{
+//   console.log(count);
+// }
+
+// 6 - misol
+// let a = 20;
+// let b = 25;
+// if(a > b){
+//   console.log(a);
+// }else{
+//   console.log(b)
+// }
+
+// 7 - misol
+// let a = 20;
+// let b = 25;
+// if(a < b){
+//   console.log(a);
+// }else{
+//   console.log(b)
+// }
+
+// 8 - misol
+// let a = 20;
+// let b = 25;
+// if(a > b){
+//   console.log(a);
+// }else if(a < b){
+//   console.log(b)
+// }else{
+//   console.log(0)
+// }
+
+// 12 - misol
+// let a = 20;
+// let b = 25;
+// let c = 30;
+// if(a < b){
+//   console.log(a);
+// }else if(b < c){
+//   console.log(b)
+// }else{
+//   console.log(c);
+  
+// }
+
+// 13 - misol
+// let a = 12;
+// let b = 13;
+// let c = 10;
+// if ((a > b && a < c) || (a < b && a > c)){
+//   console.log(a);
+// }else if ((b > a && b < c) || (b < a && b > c)){
+//   console.log(b);
+// }else{
+// console.log(c);
+// }
+
+// 14 - misol
